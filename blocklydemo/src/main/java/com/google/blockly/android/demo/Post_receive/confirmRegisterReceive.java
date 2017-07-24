370db87777ebce2162f9f59dfef62dec0578f0ac
Axe <983532911@qq.com>
2017-07-24 14:55:21 +0800

    7/24 bluetooth change

---
+++ blocklydemo/src/main/java/com/google/blockly/android/demo/Post_receive/confirmRegisterReceive.java
@@ -0,0 +1,35 @@
+package com.google.blockly.android.demo.Post_receive;
+
+/**
+ * Created by admin on 2017/7/24.
+ */
+
+public class confirmRegisterReceive {
+    public String  status;
+    public String  errMsg;
+    public String  remainNum;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
+
+    public String getRemainNum() {
+        return remainNum;
+    }
+
+    public void setRemainNum(String remainNum) {
+        this.remainNum = remainNum;
+    }
+}
